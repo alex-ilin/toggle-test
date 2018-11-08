dbe9a50a9838327f3d785bc1a4677c8752efbb5c
Alexander Ilin <alexander.ilin@rield-gmbh.eu>
2018-11-08 15:04:47 +0100

    Remove trailing whitespace from all files

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Test Toggle is similar to test toggle functionality provided by IntelliJ and oth
 
 **Features:**
 * Test Toggle allows you quick navigate between test and source files, without having to remember file paths.
-* It creates the the test or source file you are trying to navigate to (along the entire directory hierarchy) 
+* It creates the the test or source file you are trying to navigate to (along the entire directory hierarchy)
   if it does not already exist.
-* Sometimes there might be mutiple test files for a given source (Ex: Unit and Integration test). Test Toggle 
-  presents the user with choices whenever there is multiple matches and navigates to the chosen file 
+* Sometimes there might be mutiple test files for a given source (Ex: Unit and Integration test). Test Toggle
+  presents the user with choices whenever there is multiple matches and navigates to the chosen file
   (creating it of required).
 * Test Toggle is language/tech stack agnostic. So it can work with your Rails, Python, Clojure or any other project.
 * You can work with multiple such projects at the same time.
@@ -58,40 +58,40 @@ This is the recommended key binding for toggle test.
 
 Configuring
 ------------
-Before you can start using Test Toggle you need to tell Test Toggle about your projects, you can do this by configuring 
-the variable *tgt-projects*. The other optional configuration is *tgt-open-in-new-window* which controls where the 
+Before you can start using Test Toggle you need to tell Test Toggle about your projects, you can do this by configuring
+the variable *tgt-projects*. The other optional configuration is *tgt-open-in-new-window* which controls where the
 files are opened. The rest of this section describes these 2 variables.
 
 **tgt-projects:**
 
-This configuration allows Toggle Test to understand the project structure and naming conventions. tgt-projects is a list 
+This configuration allows Toggle Test to understand the project structure and naming conventions. tgt-projects is a list
 where each item is a project configuration. Here is how you add to this list.
 
 ```lisp
   (add-to-list 'tgt-projects '((:root-dir <root directory of project>)
-                              (:src-dirs <list source folders relative to root>) 
+                              (:src-dirs <list source folders relative to root>)
                               (:test-dirs <list of test folders relative to root>)
-                              (:test-prefixes <optional list of prefix strings that are added on source file names 
+                              (:test-prefixes <optional list of prefix strings that are added on source file names
                                             to get test file names>)
-                              (:test-suffixes <optional list of suffix strings without the file extension>))) 
+                              (:test-suffixes <optional list of suffix strings without the file extension>)))
 ```
 
-Each project that you configure is an alist with *:root-dir, :src-dirs, :test-dirs* as mandatory and 
-*:test-prefixes, :test-suffixes* as optional entries. 
+Each project that you configure is an alist with *:root-dir, :src-dirs, :test-dirs* as mandatory and
+*:test-prefixes, :test-suffixes* as optional entries.
 
-**Assumptions:** Test Toggle makes 2 assumptions about the all projects defined. These are not tricky assumptions. 
+**Assumptions:** Test Toggle makes 2 assumptions about the all projects defined. These are not tricky assumptions.
 Most projects have folder organization and naming conventions that complement these assumptions
 
 * The test and source directory structure (tree hierarchy) is similar.
-* The test file name can be derived by adding some prefix or suffix to source file name. 
+* The test file name can be derived by adding some prefix or suffix to source file name.
 
-  Ex: if the source file is src/controllers/user_contoller.py then the test is 
+  Ex: if the source file is src/controllers/user_contoller.py then the test is
       test/controllers/user_controller_test.py. Note that the :test-suffixes here is "_test"
 
 
 **Basic Example**
 - - - - - - - - -
-For a python project follows coding convention of prefixing "test_" for test names and has a directory structure 
+For a python project follows coding convention of prefixing "test_" for test names and has a directory structure
 that look like this
 ```
 py-proj
@@ -105,7 +105,7 @@ py-proj
 The configuration for this project would look look this
 ```lisp
 (add-to-list 'tgt-projects '((:root-dir "~/py-proj")
-                              (:src-dirs "src") 
+                              (:src-dirs "src")
                               (:test-dirs "tests")
                               (:test-prefixes "test_")))
 ```
@@ -152,11 +152,11 @@ Based on these observations about the project:
 The best configuration for this project is,
 ```lisp
 (add-to-list 'tgt-projects '((:root-dir "~/polyglot-proj")
-                              (:src-dirs "play/app" "foo-lib/src") 
+                              (:src-dirs "play/app" "foo-lib/src")
                               (:test-dirs "play/test" "foo-lib/test" "integration-specs")
                               (:test-suffixes "Spec" "$Spec")))
 (add-to-list 'tgt-projects '((:root-dir "~/polyglot-proj/bar-lib")
-                              (:src-dirs "src") 
+                              (:src-dirs "src")
                               (:test-dirs "test")
                               (:test-suffixes "Test")))
 
@@ -169,35 +169,35 @@ Note that the potential candidates for *foo-lib/src/Foo.scala* are
 * integration-specs/test/FooSpec.scala
 * integration-specs/test/Foo$Spec.scala
 
-The best match is *foo-lib/test/FooSpec.scala* as this file already exists. 
+The best match is *foo-lib/test/FooSpec.scala* as this file already exists.
 
-If this file did not exist, then the user will be presented with all the options and the selected file is automatically 
+If this file did not exist, then the user will be presented with all the options and the selected file is automatically
 created.
 
 
 **Adding new projects vs adding new src/test directories:**
 
-Note that in the configuration in the second example defines separate projects for *bar-lib*, while *foo-lib* 
-is just extra entries in *:src-dirs* and *:test-dirs*. It is recommended that you define many smaller projects 
-whenever possible. If a module is independent and contains all tests inside module directory then it is better 
-to add it as a separate tgt-project. A separate project cannot be defined in case of *foo-lib* because 
-integration-specs depends on both *foo-lib* and the Play application 
+Note that in the configuration in the second example defines separate projects for *bar-lib*, while *foo-lib*
+is just extra entries in *:src-dirs* and *:test-dirs*. It is recommended that you define many smaller projects
+whenever possible. If a module is independent and contains all tests inside module directory then it is better
+to add it as a separate tgt-project. A separate project cannot be defined in case of *foo-lib* because
+integration-specs depends on both *foo-lib* and the Play application
 
 
 **tgt-open-in-new-window:**
 
-This setting controls where the toggle file opens up. A non nil value opens the toggle file in a new window, 
-so you can do a side-by-side edit of source and test files. A nil value replaces the current window content. 
-The default value is **t**  
+This setting controls where the toggle file opens up. A non nil value opens the toggle file in a new window,
+so you can do a side-by-side edit of source and test files. A nil value replaces the current window content.
+The default value is **t**
 ```lisp
   (setq tgt-open-in-new-window <'nil or t>)
 ```
 
 Contributing
 --------------------------
-* Feel free to [fork](https://help.github.com/articles/fork-a-repo) the [repo](https://github.com/rags/toggle-test), 
-make changes and raise [pull request](https://help.github.com/articles/using-pull-requests) once you are done. 
-Toggle Test uses [ert](http://www.gnu.org/software/emacs/manual/html_mono/ert.html) for unit/intergration tests. 
+* Feel free to [fork](https://help.github.com/articles/fork-a-repo) the [repo](https://github.com/rags/toggle-test),
+make changes and raise [pull request](https://help.github.com/articles/using-pull-requests) once you are done.
+Toggle Test uses [ert](http://www.gnu.org/software/emacs/manual/html_mono/ert.html) for unit/intergration tests.
 Keep them green and test your changes.
 * You can also contribute by reporting any bugs or feature requests in the [issues section](https://github.com/rags/toggle-test/issues)
 
@@ -206,4 +206,3 @@ License
 [GNU General Public License](http://www.gnu.org/licenses/)
 
 Copyright (C) 2014 [Raghunandan Rao](mailto:r.raghunandan@gmail.com)
-
--- toggle-test.el
+++ toggle-test.el
@@ -21,13 +21,13 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; Toggle test provides test toggle functionality very similar to Jetbrains 
+;; Toggle test provides test toggle functionality very similar to Jetbrains
 ;; IDEs like IntelliJ/Rubymine/Pycharm. It presents
-;; the user with choices in case there are multiple macthes (Ex: You have 
-;; integration and unit test for the same source file). It creates the file 
+;; the user with choices in case there are multiple macthes (Ex: You have
+;; integration and unit test for the same source file). It creates the file
 ;; (test or source), along with the entire directory hierarchy if the file does
-;; not exist. 
-;; It is language agnostic so you can use it on your rails, django, scala, node.js 
+;; not exist.
+;; It is language agnostic so you can use it on your rails, django, scala, node.js
 ;; or any other projects.
 
 ;;; Change log:
@@ -51,31 +51,31 @@
 ;; 3. Test source folder(s) (relative to root directory)
 ;; 4. prefix/suffix attached to source file names to name the test files
 ;;    Ex: src/foo.py <-> test/test_foo.py - the prefix is 'test_'
-;;        src/controllers/Foo.scala <-> specs/controllers/Foo$Spec.scala 
+;;        src/controllers/Foo.scala <-> specs/controllers/Foo$Spec.scala
 ;;        The suffix here is '$Spec'.
 ;;    Note: you can specify both prefix and suffix if required.
 ;;
 ;; Usage:
-;; (add-to-list 'tgt-projects '((:root-dir "~/python-project") 
-;;                             (:src-dirs "src") (:test-dirs "tests") 
-;;                             (:test-prefixes "test-")))          
-;; (add-to-list 'tgt-projects '((:root-dir "~/scala-project") 
-;;                             (:src-dirs "src") (:test-dirs "specs") 
+;; (add-to-list 'tgt-projects '((:root-dir "~/python-project")
+;;                             (:src-dirs "src") (:test-dirs "tests")
+;;                             (:test-prefixes "test-")))
+;; (add-to-list 'tgt-projects '((:root-dir "~/scala-project")
+;;                             (:src-dirs "src") (:test-dirs "specs")
 ;;                             (:test-suffixes "$Spec")))
 
 ;;;###autoload
-(defcustom tgt-projects '() 
-  "Project entries. 
+(defcustom tgt-projects '()
+  "Project entries.
 One entry per project that provides naming convention and folder structure"
   :group 'toggle-test
   :type '(repeat (alist)))
 
-;; Indicates if the toggle file should be opened in a new window 
+;; Indicates if the toggle file should be opened in a new window
 ;; or replace the buffer in current window. The default behavior is to open it in new window
 ;; Use (setq tgt-open-in-new-window 'nil) to override default behavior
 
 ;;;###autoload
-(defcustom tgt-open-in-new-window t 
+(defcustom tgt-open-in-new-window t
   "Indicates if the files are opened in new window or current window"
   :group 'toggle-test
   :type 'boolean)
@@ -83,25 +83,25 @@ One entry per project that provides naming convention and folder structure"
 (defun tgt-proj-prop (prop proj) (cdr (assoc prop proj)))
 (defun tgt-root-dir (proj) (file-truename (car (tgt-proj-prop :root-dir proj))))
 
-(defun tgt-relative-file-path (file proj dir-type) 
-  (reduce 
-   (lambda (cur_val dir) 
-     (or cur_val 
-	 (let ((src-dir (file-name-as-directory 
-			 (expand-file-name dir (tgt-root-dir proj))))) 
+(defun tgt-relative-file-path (file proj dir-type)
+  (reduce
+   (lambda (cur_val dir)
+     (or cur_val
+	 (let ((src-dir (file-name-as-directory
+			 (expand-file-name dir (tgt-root-dir proj)))))
 	   (if (tgt-is-ancestor-p src-dir file)
-	       (subseq file (length src-dir)))))) 
+	       (subseq file (length src-dir))))))
    (cdr (assoc dir-type proj))
    :initial-value 'nil))
 
-;; Given a file return its project 
+;; Given a file return its project
 (defun tgt-proj-for (file)
   (tgt-best-project (remove-if-not
 		(lambda (proj) (tgt-is-ancestor-p (tgt-root-dir proj) file)) tgt-projects)))
 
 (defun tgt-best-project (projects)
-  (if projects 
-	  (reduce (lambda (res proj) 
+  (if projects
+	  (reduce (lambda (res proj)
 				(if (> (tgt-root-depth proj) (tgt-root-depth res)) proj res)) projects)
 	'nil))
 
@@ -112,28 +112,28 @@ One entry per project that provides naming convention and folder structure"
   (assert (tgt-proj-prop :src-dirs proj) nil "Source directory not configured")
   (assert (tgt-proj-prop :test-dirs proj) nil "Test directory not configured")
   (let ((test-file-rel-path (tgt-relative-file-path file proj :test-dirs)))
-    (if test-file-rel-path 
+    (if test-file-rel-path
 	(values 'nil test-file-rel-path)
       (values (tgt-relative-file-path file proj :src-dirs) 'nil))))
 
 
-(defun tgt-find-match (file) 
+(defun tgt-find-match (file)
   (let ((proj (tgt-proj-for file)))
-      (cond (proj 
-	   (multiple-value-bind 
-	       (src-file-rel-path test-file-rel-path) 
+      (cond (proj
+	   (multiple-value-bind
+	       (src-file-rel-path test-file-rel-path)
 	       (tgt-find-project-file-in-dirs file proj)
 	     (cond
-	      (test-file-rel-path (tgt-all-toggle-paths 
-							   test-file-rel-path proj :src-dirs 
+	      (test-file-rel-path (tgt-all-toggle-paths
+							   test-file-rel-path proj :src-dirs
 							   #'tgt-possible-src-file-names))
-	      (src-file-rel-path (tgt-all-toggle-paths 
-							  src-file-rel-path proj :test-dirs 
+	      (src-file-rel-path (tgt-all-toggle-paths
+							  src-file-rel-path proj :test-dirs
 							  #'tgt-possible-test-file-names))
-	      
+
 	      (t (message "File '%s' in project '%s' is not part src-dirs or test-dirs"
 			  file (tgt-root-dir proj)) 'nil))))
-	  (t (message "File '%s' not part of any project. Have you defined a project?" file) 
+	  (t (message "File '%s' not part of any project. Have you defined a project?" file)
 		 'nil))))
 
 (defun tgt-best-matches (all-matches)
@@ -141,11 +141,11 @@ One entry per project that provides naming convention and folder structure"
 	(if exact-matches (values t exact-matches) (values 'nil all-matches))))
 
 (defun tgt-all-toggle-paths (rel-path proj dir-type file-names-generator)
-  (tgt-make-full-paths 
-   (tgt-possible-dirs proj dir-type (or (file-name-directory rel-path) "")) 
-   (funcall file-names-generator 
-			(file-name-nondirectory rel-path) 
-			(tgt-proj-prop :test-prefixes proj) 
+  (tgt-make-full-paths
+   (tgt-possible-dirs proj dir-type (or (file-name-directory rel-path) ""))
+   (funcall file-names-generator
+			(file-name-nondirectory rel-path)
+			(tgt-proj-prop :test-prefixes proj)
 			(tgt-proj-prop :test-suffixes proj))))
 
 (defun tgt-make-full-paths (dirs filenames)
@@ -154,23 +154,23 @@ One entry per project that provides naming convention and folder structure"
 ;rel-dir-path is com/foo/bar for src in "proj-root/src/com/foo/bar/Blah.java
 (defun tgt-possible-dirs (proj dir-type rel-dir-path)
   (let ((root (tgt-root-dir proj)))
-	(mapcar (lambda (dir) (expand-file-name rel-dir-path (expand-file-name dir root))) 
+	(mapcar (lambda (dir) (expand-file-name rel-dir-path (expand-file-name dir root)))
 			(tgt-proj-prop dir-type proj))))
 
 
-(defun tgt-remove-file-prefix (prefix file) 
+(defun tgt-remove-file-prefix (prefix file)
   (if (string-match (concat "^" prefix) file) (replace-match "" t t file) 'nil))
 
 (defun tgt-remove-file-suffix (name suffix ext)
-  (if (string-match (concat suffix "$") name) 
-	  (concat (replace-match "" t t name) ext) 
+  (if (string-match (concat suffix "$") name)
+	  (concat (replace-match "" t t name) ext)
 	'nil))
 
-(defun tgt-remove-file-preffix-suffix (prefix name suffix ext) 
+(defun tgt-remove-file-preffix-suffix (prefix name suffix ext)
   (tgt-remove-file-prefix prefix (or (tgt-remove-file-suffix name suffix ext) "")))
 
 (defun tgt-possible-src-file-names (file prefixes suffixes)
-  (tgt-possible-file-names file prefixes suffixes 
+  (tgt-possible-file-names file prefixes suffixes
 						   #'tgt-remove-file-prefix
 						   #'tgt-remove-file-suffix
 						   #'tgt-remove-file-preffix-suffix))
@@ -182,34 +182,34 @@ One entry per project that provides naming convention and folder structure"
   ;(append seq 'nil) converts vectors/sequences/list to list
   (delete-dups (remove 'nil (append seq 'nil))))
 
-(defun tgt-possible-file-names (file prefixes suffixes 
+(defun tgt-possible-file-names (file prefixes suffixes
 									 pref-fn suff-fn
 									 pref-suff-fn)
-  (or (tgt-santize-seq 
+  (or (tgt-santize-seq
 	   (let ((name (file-name-sans-extension file))
 			 (ext (file-name-extension file t))
 			 (ret-val '()))
-		 (setq ret-val (vconcat 
+		 (setq ret-val (vconcat
 						(mapcar (lambda (prefix) (funcall pref-fn prefix file)) prefixes)
 						(mapcar (lambda (suffix) (funcall suff-fn name suffix ext)) suffixes)))
-	 
-		 (if (and prefixes suffixes) 
-			 (setq ret-val (vconcat (tgt-cross-join 
-									 prefixes suffixes 
-									 (lambda (prefix suffix) 
-									   (funcall pref-suff-fn prefix name suffix ext))) 
+
+		 (if (and prefixes suffixes)
+			 (setq ret-val (vconcat (tgt-cross-join
+									 prefixes suffixes
+									 (lambda (prefix suffix)
+									   (funcall pref-suff-fn prefix name suffix ext)))
 									ret-val)))
-	 
+
 		 ret-val)) (list file)))
 
-;join 2 lists - to make list of tuples by default. 
+;join 2 lists - to make list of tuples by default.
 ;fn argument can override how 2 elements join
 (defun tgt-cross-join (list1 list2 &optional fn)
   (cond ((not list1) list2)
 		((not list2) list1)
-		(t (let ((ret-val '())) 
+		(t (let ((ret-val '()))
 			 (dolist (i list1)
-			  (dolist (j list2) 
+			  (dolist (j list2)
 				(add-to-list 'ret-val (if fn (funcall fn i j) (list i j)))))
 			 ret-val))))
 
@@ -222,16 +222,16 @@ One entry per project that provides naming convention and folder structure"
 
 
 (defun tgt-show-matches (matches exact-match-p)
-  (with-output-to-temp-buffer "*Toggle Test*"  
-	(funcall (if tgt-open-in-new-window 
+  (with-output-to-temp-buffer "*Toggle Test*"
+	(funcall (if tgt-open-in-new-window
 				 #'switch-to-buffer-other-window #'switch-to-buffer) "*Toggle Test*")
-	(princ (if exact-match-p 
+	(princ (if exact-match-p
 			   "Mutiple matching files were found. Choose one to open:\n"
 			 "No matching file found. These are the potentials. Pick one to create:\n"))
 	(dolist (file matches)
 	  (princ "* ")
-	  (insert-button file 'action (lambda (btn) 
-									(tgt-find-file 
+	  (insert-button file 'action (lambda (btn)
+									(tgt-find-file
 									 (button-label btn) #'find-alternate-file)))
 	  (princ "\n"))))
 
@@ -246,15 +246,15 @@ One entry per project that provides naming convention and folder structure"
 ;;;###autoload
 (defun tgt-toggle ()
   (interactive)
-  (if buffer-file-truename  
-; expand ~/ with 
+  (if buffer-file-truename
+; expand ~/ with
 	  (tgt-open (tgt-find-match (file-truename buffer-file-truename)))))
 
 (defun tgt-is-ancestor-p (dir file)
   (if (and dir file (> (length file) 0) (> (length dir) 0))
-   (let ((dir-name (file-name-as-directory (expand-file-name dir))) 
+   (let ((dir-name (file-name-as-directory (expand-file-name dir)))
 	 (file-name (expand-file-name file)))
-	 (and (>= (length file-name) (length dir-name)) 
+	 (and (>= (length file-name) (length dir-name))
 	      (string= (substring file-name 0 (length dir-name)) dir-name))) 'nil))
 
 (provide 'toggle-test)
